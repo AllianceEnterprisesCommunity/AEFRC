fe388c937de39adaa02375384cd84d1528ea5877
AllianceEnterprisesCommunity <30439600+AllianceEnterprisesCommunity@users.noreply.github.com>
2017-12-05 10:41:05 -0800

    Update Craig2.ps1

--- Craig2.ps1
+++ Craig2.ps1
@@ -1,6 +1,6 @@
 workflow Set-RunbookConfiguration
 {
     Write-Progress -Activity "This is a progress stream..."
-    Write-Verbose -Message "This is a verbose message..."
+    Write-Verbose -Message "This is a very verbose message..."
     Write-Ouptut "Generating output"
 }
