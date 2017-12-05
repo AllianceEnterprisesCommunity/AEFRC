28020804d33bd5b7031506e4d356ab47f3f0d51c
AllianceEnterprisesCommunity <30439600+AllianceEnterprisesCommunity@users.noreply.github.com>
2017-12-05 10:18:00 -0800

    Updated Craig2.ps1 in Azure Automation.

--- Craig2.ps1
+++ Craig2.ps1
@@ -1,1 +1,6 @@
-# Powershell Runbook
+workflow Set-RunbookConfiguration
+{
+    Write-Progress -Activity "This is a progress stream..."
+    Write-Verbose -Message "This is a verbose message..."
+    Write-Ouptut "Generating output"
+}
